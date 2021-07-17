5428ae595891676e1a46d2bc3b2c5628eb9b65ca
nicholas-boyson <58054228+nicholas-boyson@users.noreply.github.com>
2021-07-17 13:09:28 +1000

    test support for user search

--- internal/users/users.go
+++ internal/users/users.go
@@ -73,77 +73,62 @@ func SearchUsers(users []User, ident string, value string) (userList []User) {
 		case "_id":
 			if strconv.Itoa(user.Id) == value {
 				userList = append(userList, user)
-				return
 			}
 		case "url":
 			if user.URL == value {
 				userList = append(userList, user)
-				return
 			}
 		case "external_id":
 			if user.ExternalId == value {
 				userList = append(userList, user)
-				return
 			}
 		case "name":
 			if user.Name == value {
 				userList = append(userList, user)
-				return
 			}
 		case "alias":
 			if user.Alias == value {
 				userList = append(userList, user)
-				return
 			}
 		case "created_at":
 			if user.CreatedAt == value {
 				userList = append(userList, user)
-				return
 			}
 		case "active":
 			if strconv.FormatBool(user.Active) == value {
 				userList = append(userList, user)
-				return
 			}
 		case "verified":
 			if strconv.FormatBool(user.Verified) == value {
 				userList = append(userList, user)
-				return
 			}
 		case "shared":
 			if strconv.FormatBool(user.Shared) == value {
 				userList = append(userList, user)
-				return
 			}
 		case "locale":
 			if user.Locale == value {
 				userList = append(userList, user)
-				return
 			}
 		case "timezone":
 			if user.Timezone == value {
 				userList = append(userList, user)
-				return
 			}
 		case "last_login_at":
 			if user.LastLoginAt == value {
 				userList = append(userList, user)
-				return
 			}
 		case "email":
 			if user.Email == value {
 				userList = append(userList, user)
-				return
 			}
 		case "phone":
 			if user.Phone == value {
 				userList = append(userList, user)
-				return
 			}
 		case "signature":
 			if user.Signature == value {
 				userList = append(userList, user)
-				return
 			}
 		case "organization_id":
 			if strconv.Itoa(user.OrganizationId) == value {
@@ -153,18 +138,15 @@ func SearchUsers(users []User, ident string, value string) (userList []User) {
 			for _, tag := range user.Tags {
 				if tag == value {
 					userList = append(userList, user)
-					return
 				}
 			}
 		case "suspended":
 			if strconv.FormatBool(user.Suspended) == value {
 				userList = append(userList, user)
-				return
 			}
 		case "role":
 			if user.Role == value {
 				userList = append(userList, user)
-				return
 			}
 		default:
 			return
